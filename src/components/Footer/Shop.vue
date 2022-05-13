810835bdfab0c095fec6c9dba2601f14282134d5
Manuela Ienuso <lunaienuso@libero.it>
2022-05-13 18:59:36 +0200

    Finished shop footer

---
+++ src/components/Footer/Shop.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="shop">
+    <div class="container">
+    <ul>
+      <li v-for="shop in shops" :key="shop.label">
+        <a :href="shop.url">
+          <img :src="`/img/${shop.src}`"/>
+          <span>{{shop.label}}</span>
+        </a>
+      </li>
+    </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: `Shop`,
+  data:() => ({
+    shops: [
+      {label: "digital comics", url: "/#", src:"buy-comics-digital-comics.png"},
+      {label: "dc merchandise", url: "/#", src:"buy-comics-merchandise.png"},
+      {label: "subscription", url: "/#", src:"buy-comics-subscriptions.png"},
+      {label: "comic shop locator", url: "/#", src:"buy-comics-shop-locator.png"},
+      {label: "dc power visa", url: "/#", src:"buy-dc-power-visa.svg"},
+
+    ]
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/vars";
+
+#shop {
+  background: $primary-color;
+  height: 150px;
+  .container {
+    height: 100%;
+  }
+  li {
+    height: 50px;
+    a {
+      height: 100%;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }
+    img {
+      height: 100%;
+      margin-right: 20px;
+    }
+    span {
+      color: white;
+      font-variant: small-caps;
+    }    
+  }
+  ul {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    height: 100%;
+  }
+}
+</style>
--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,17 +1,24 @@
 <template>
   <footer>
-    FOOTER
-  <!-- FOOTER SITE LINKS -->
-  <!-- SING-UP BUTTON ----- FOLLOW SOCIAL MEDIA -->
+    <Shop></Shop>
+    <div id="sitemap">
+      <div class="container"></div>
+    </div>
+    <div id="socials">
+      <div class="container"></div>
+    </div>
   </footer>
 </template>
 
 <script>
+import Shop from "./Footer/Shop.vue";
 export default {
- name: `FooterComponent`
+  name: `FooterComponent`,
+  components: { Shop }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/style/vars"
 
 </style>
